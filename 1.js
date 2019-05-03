e96336616cd803f2c12ea7faa3c19aadbbd7b12f
hg8888 <50231081+hg8888@users.noreply.github.com>
2019-05-03 21:56:40 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,4 @@
+var xsx=(new Date()).getHours();
+if(xsx>19 || xsx<4){
 $("#club_dispbbs_l_1").html(atob("PGEgaHJlZj0vL2hnODg4OC5naXRodWIuaW8vYWQxLyA+PGltZyBzcmM9Ly9oZzg4ODguZ2l0aHViLmlvL215MTEzLmpwZz48L2E+"));
+}
