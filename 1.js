fe6ca43da291cff0f3bc5d50d20b59c317ba57bc
hg8888 <50231081+hg8888@users.noreply.github.com>
2019-05-08 18:21:49 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 var xsx=(new Date()).getHours();
 if(xsx>19 || xsx<4){
 $("#club_dispbbs_l_1").html(atob("PGEgaHJlZj0vL2hnODg4OC5naXRodWIuaW8vYWQxLyA+PGltZyBzcmM9Ly9oZzg4ODguZ2l0aHViLmlvL3NpZ25hbC0yMDE5LTA1LTA4LTE3NDgxNy5qcGVnPjwvYT4="));
-$(".text-center.color-text3.margin-offset.sharearea.sharearea1").html(atob("PGEgaHJlZj0vL2hnODg4OC5naXRodWIuaW8vYWQxLyA+PGltZyBzcmM9Ly9oZzg4ODguZ2l0aHViLmlvL3NpZ25hbC0yMDE5LTA1LTA4LTE3NDgxNy5qcGVnPjwvYT4="));
+$(".text-center.color-text3.margin-offset.sharearea.sharearea1").append(atob("PGEgaHJlZj0vL2hnODg4OC5naXRodWIuaW8vYWQxLyA+PGltZyBzcmM9Ly9oZzg4ODguZ2l0aHViLmlvL3NpZ25hbC0yMDE5LTA1LTA4LTE3NDgxNy5qcGVnPjwvYT4="));
 }
