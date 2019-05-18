b069c2517ee60d0cdb9bcfa228145cb09a283f65
hg8888 <50231081+hg8888@users.noreply.github.com>
2019-05-18 20:22:36 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,5 +1,2 @@
-var xsx=(new Date()).getHours();
-//if(xsx>19 || xsx<4){
-jQuery(".new-share-main").append(atob("PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9oYW9jaGUyLmdpZj48L2E+"));
-jQuery(".text-center.color-text3.margin-offset.sharearea.sharearea1").append(atob("PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9oYW9jaGUyLmdpZiAgIHN0eWxlPSJoZWlnaHQ6IGF1dG87IG1heC13aWR0aDogMTAwJTsiPjwvYT4="));
-//}
+jQuery(".donate-more").append(atob("PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9hZDEvaGFvY2hlMi5naWY+PC9hPg=="));
+jQuery(".text-center.color-text3.margin-offset.sharearea.sharearea1").append(atob("PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9hZDEvaGFvY2hlMi5naWYgICBzdHlsZT0iaGVpZ2h0OiBhdXRvOyBtYXgtd2lkdGg6IDEwMCU7Ij48L2E+"));
