e042ff261ab6c6ef7f8bc7eb4c0305f4077dbd91
hg8888 <50231081+hg8888@users.noreply.github.com>
2019-05-21 19:46:49 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,4 +1,4 @@
 var xshtml="PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9hZDEvaGFvY2hlMi5naWY+PC9hPg==";
-jQuery("#club_dispbbs_l_1").html(atob(xshtml));
-jQuery(".donate-more").append(atob(xshtml));
+jQuery("#club_dispbbs_l_15555").html(atob(xshtml));
+jQuery(".donate-more5555").append(atob(xshtml));
 jQuery(".text-center.color-text3.margin-offset.sharearea.sharearea1").append(atob("PGEgaHJlZj0vL2phY2tzb24zOC5naXRodWIuaW8gPjxpbWcgc3JjPS8vaGc4ODg4LmdpdGh1Yi5pby9hZDEvaGFvY2hlMi5naWYgICBzdHlsZT0iaGVpZ2h0OiBhdXRvOyBtYXgtd2lkdGg6IDEwMCU7Ij48L2E+"));
